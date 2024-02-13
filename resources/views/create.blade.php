ab81916c167bf0400072b8841febd1dd889dd5ab
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-02-13 09:14:15 +0100

    feat:readme

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.admin')
 
 @section('content')
-@include('partials.errors')
-<form>
-    <h1 class="text-warning">New Project</h1>
-    @csrf
-    
-    <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
-    <div class="mb-3">
-      <label for="title" class="form-label text-warning">Project Name</label>
-      <input type="text" class="form-control" id="title" aria-describedby="title" value="{{old('title')}}">
-    </div>
-    <div class="mb-3">
-      <label for="description" class="form-label text-warning">Description</label>
-      <textarea class="form-control " name="content" id="content" cols="30" rows="10">{{old('content')}}</textarea>
-    </div>
-    <div class="mb-3">
-    <label for="type" class="form-label text-warning">Language Type</label>
-      <select class="form-select" aria-label="Default select example" name="type_id">
-        <option selected>Please choose the language used:</option>
-       
-        @foreach ($types as $type)
-            <option value="{{$type->id}}">{{$type->title}}</option>
-        @endforeach
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="technology" class="form-label text-warning">Technology</label>
-      @foreach($technologies as $tech)
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="tag-{{$tag->id}}" value="{{$tech->id}}" name="technologies" 
-        {{in_array($tech->id,old('technologies',[])) ? 'checked': ''}}>
-        <label class="form-check-label" for="tag-{{$tag->id}}">{{$tech->title}}</label>
-      </div>
-      @endforeach
-    </div>
-    <div class="mb-3">
-      <label for="post_image" class="form-label text-warning">Add an Image to your Project</label>
-      <input class="form-control" type="file" id="post_image" name="post_image">
-  </div>
-    <button type="submit" class="btn btn-warning">Submit the entry</button>
-  </form>
+    @include('partials.errors')
+    <form>
+        <h1 class="text-warning">New Project</h1>
+        @csrf
 
-@endsection
+        <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="title" class="form-label text-warning">Project Name</label>
+                <input type="text" class="form-control" id="title" aria-describedby="title" value="{{ old('title') }}">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label text-warning">Description</label>
+                <textarea class="form-control " name="content" id="content" cols="30" rows="10">{{ old('content') }}</textarea>
+            </div>
+            <div class="mb-3">
+                <label for="type" class="form-label text-warning">Language Type</label>
+                <select class="form-select" aria-label="Default select example" name="type_id">
+                    <option selected>Please choose the language used:</option>
+
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}">{{ $type->title }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="technology" class="form-label text-warning">Technology</label>
+                @foreach ($technologies as $tech)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="tag-{{ $tag->id }}"
+                            value="{{ $tech->id }}" name="technologies"
+                            {{ in_array($tech->id, old('technologies', [])) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="tag-{{ $tag->id }}">{{ $tech->title }}</label>
+                    </div>
+                @endforeach
+            </div>
+            <div class="mb-3">
+                <label for="post_image" class="form-label text-warning">Add an Image to your Project</label>
+                <input class="form-control" type="file" id="post_image" name="post_image">
+            </div>
+            <button type="submit" class="btn btn-warning">Submit the entry</button>
+        </form>
+    @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,47 +7,47 @@
         @csrf
 
         @method('PUT')
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label text-warning">Project Name</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$project->title}}">
-    </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label text-warning">Description</label>
-      <textarea class="form-control " name="content" id="" cols="30" rows="10">{{$project->content}}</textarea>
-    </div>
-    <div class="mb-3">
-      <label for="type" class="form-label text-warning">Language Type</label>
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Please choose the language used:</option>
-          <option value="Html">Html</option>
-          <option value="Js">Js</option>
-          <option value="Css">Css</option>
-          <option value="Php">Php</option>
-        </select>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label text-warning">Project Name</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$project->title}}">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label text-warning">Description</label>
+        <textarea class="form-control " name="content" id="" cols="30" rows="10">{{$project->content}}</textarea>
+      </div>
+      <div class="mb-3">
+        <label for="type" class="form-label text-warning">Language Type</label>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Please choose the language used:</option>
+            <option value="Html">Html</option>
+            <option value="Js">Js</option>
+            <option value="Css">Css</option>
+            <option value="Php">Php</option>
+          </select>
       </div>
       <div class="mb-3">
         <label for="technology" class="form-label text-warning">Technology</label>
-        @foreach($technologies as $tech)
-        @if ($errors->any())
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="tag-{{$tag->id}}" value="{{$tech->id}}" name="technologies" 
-          {{$project->technologies->contain($tech->id) ? 'checked': ''}}>
-          <label class="form-check-label" for="tag-{{$tag->id}}">{{$tech->title}}</label>
-        </div>
+          @foreach($technologies as $tech)
+          @if ($errors->any())
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="tag-{{$tag->id}}" value="{{$tech->id}}" name="technologies" 
+            {{$project->technologies->contain($tech->id) ? 'checked': ''}}>
+            <label class="form-check-label" for="tag-{{$tag->id}}">{{$tech->title}}</label>
+          </div>
           @else
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="tag-{{$tag->id}}" value="{{$tech->id}}" name="technologies" 
-              {{in_array($tech->id,old('technologies',[])) ? 'checked': ''}}>
-              <label class="form-check-label" for="tag-{{$tag->id}}">{{$tech->title}}</label>
-            </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="tag-{{$tag->id}}" value="{{$tech->id}}" name="technologies" 
+            {{in_array($tech->id,old('technologies',[])) ? 'checked': ''}}>
+            <label class="form-check-label" for="tag-{{$tag->id}}">{{$tech->title}}</label>
+          </div>
           @endif
-        </div>
-        @endforeach
+      </div>
+      @endforeach
       </div>
       <div class="mb-3">
         <label for="post_image" class="form-label text-warning">Add an Image to your Project</label>
         <input class="form-control" type="file" id="post_image" name="post_image">
-    </div>
-    <button type="submit" class="btn btn-warning">Submit</button>
-  </form>
+      </div>
+      <button type="submit" class="btn btn-warning">Submit</button>
+</form>
 @endsection
